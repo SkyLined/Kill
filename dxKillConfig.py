a1bd9ada1b63a088c7b051607a5f452f9fb078f7
SkyLined <berendjanwever@gmail.com>
2015-10-16 11:10:27 +0200

    fix kill binary path in dxKillConfig

--- dxKillConfig.py
+++ dxKillConfig.py
@@ -10,8 +10,8 @@ except:
 dxKillConfig = dxConfig.setdefault("Kill", {});
 # Add default values where no values have been supplied:
 for (sName, xValue) in {
-  "sKillBinaryPath_x86": os.path.join(sBaseDir, "Kill", "bin", "Kill_x86.exe"),
-  "sKillBinaryPath_x64": os.path.join(sBaseDir, "Kill", "bin", "Kill_x64.exe"),
+  "sKillBinaryPath_x86": os.path.join(sBaseDir, "bin", "Kill_x86.exe"),
+  "sKillBinaryPath_x64": os.path.join(sBaseDir, "bin", "Kill_x64.exe"),
 }.items():
   if sName not in dxKillConfig:
     dxKillConfig[sName] = xValue;
