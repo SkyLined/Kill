74df9c0c7550f648854f021cf34b0eccdcdf06d8
SkyLined <berendjanwever@gmail.com>
2015-10-30 00:04:21 +0100

    EdgeDbg => Kill (copied code, forgot to rename)

--- src/fuKill.h
+++ src/fuKill.h
@@ -10,13 +10,13 @@ UINT fuKill(UINT uArgumentsCount, _TCHAR* asArguments[]) {
     _tprintf(_T("- Cannot determine if this is a 64-bit version of Windows (error %08X).\r\n"), GetLastError());
     bSuccess = FALSE;
   } else if (bIsWow64) {
-    _tprintf(_T("- The 32-bit build of EdgeDbg does not work on a 64-bit build of Windows.\r\n"));
+    _tprintf(_T("- The 32-bit build of Kill does not work on a 64-bit build of Windows.\r\n"));
     bSuccess = FALSE;
   } else if (uArgumentsCount < 2) {
     _tprintf(_T("Usage:\r\n"));
     _tprintf(_T("  Kill target [another_target [...]]\r\n"));
     _tprintf(_T("Where target is either a process id or binary file name.\r\n"));
-    _tprintf(_T("See documentation at https://github.com/SkyLined/EdgeDbg/ for more details.\r\n"));
+    _tprintf(_T("See documentation at https://github.com/SkyLined/Kill/ for more details.\r\n"));
     bSuccess = FALSE;
   } else {
     for (UINT uArgumentIndex = 1; uArgumentIndex < uArgumentsCount; uArgumentIndex++) {
